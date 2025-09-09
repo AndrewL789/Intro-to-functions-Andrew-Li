001003a9f5e74fdfeb5407a553e166a0ecae2c5e
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-09 20:34:42 +0000

    probably done

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+
 
 def message(input):
     print(input)
@@ -13,13 +13,13 @@ def message(input):
 def square(x):
     t.forward(x)
     t.left(90)
-    t.forward(x)
+    t.forward(100)
     t.left(90)
     t.forward(x)
     t.left(90)
-    t.forward(x)
+    t.forward(100)
     t.left(90)
-    square(200)
+    square(125)
 
 def equal(x):
     t.forward(x)
@@ -27,6 +27,14 @@ def equal(x):
     t.forward(x)
     t.left(120)
     t.forward(x)
-    equal(200)
+    equal(90)
+
+def right ():
+    t.forward(100)
+    t.lefft(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
 
 
