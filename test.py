9038f89f19f6f8f2e3e365b6f761b2ad0138c61e
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-12 09:18:01 -0400

    loops done

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ t = Turtle()
 
 
 t.shape('turtle')
-t.speed(293)
+t.speed(10)
 
 
-def square(x):
+def square(x,y):
     t.forward(x)
     t.left(90)
-    t.forward(100)
+    t.forward(x)
     t.left(90)
     t.forward(x)
     t.left(90)
-    t.forward(100)
+    t.forward(x)
     t.left(90)
-square(125)
+
 
 def equal(x):
     t.forward(x)
@@ -26,6 +26,38 @@ def equal(x):
     t.forward(x)
 equal(90)
 
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
 
 
 
+
+
+def rectangle(x,y):
+    t.forward(x)
+    t.left(90)
+    t.forward(y)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(y)
+rectangle(125,100)
+
+
+
+name = input("what is your name?")
+#use F string 
+print(f"His name is {name}")
+
+
+#inputs output strings
+
+# integers numbers
+
+a = int('5')
+bill = input("How much was the bill?")
+print(int(bill) * .15)
